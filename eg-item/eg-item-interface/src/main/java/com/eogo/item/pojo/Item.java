840012dfb2b2d392fbde24af7b95454207b888bf
zhangxiaohui4565 <zhangxh940426@163.com>
2018-11-30 19:35:37 +0800

    修改依赖

---
+++ eg-item/eg-item-interface/src/main/java/com/eogo/item/pojo/Item.java
@@ -0,0 +1,14 @@
+package com.eogo.item.pojo;
+
+import lombok.Data;
+
+/**
+ * @author zhangxh
+ * @since 1.0
+ */
+@Data
+public class Item {
+    private Integer id ;
+    private String name ;
+    private Long price ;
+}
