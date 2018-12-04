66c81d07c48a98e1ca1acc7e492e84dc92fb2034
zhangxiaohui4565 <zhangxh940426@163.com>
2018-12-04 22:46:01 +0800

    添加商品

--- eg-item/eg-item-service/src/main/java/com/eogo/ItemServiceApp.java
+++ eg-item/eg-item-service/src/main/java/com/eogo/ItemServiceApp.java
@@ -3,6 +3,7 @@ package com.eogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author zhangxh
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.eogo.item.mapper") // 扫描mapper包
 public class ItemServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(ItemServiceApp.class);
