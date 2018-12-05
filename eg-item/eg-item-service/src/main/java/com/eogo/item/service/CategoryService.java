c5dcb4680e7aeea06fc75324f602fb38ca207209
zhangxiaohui4565 <zhangxh940426@163.com>
2018-12-05 13:43:43 +0800

    商品管理模块

---
+++ eg-item/eg-item-service/src/main/java/com/eogo/item/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.eogo.item.service;
+
+import com.eogo.item.mapper.CategoryMapper;
+import com.eogo.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category> queryListByParent(Long pid) {
+        Category category = new Category();
+        category.setParentId(pid);
+        return this.categoryMapper.select(category);
+    }
+}
---
+++ eg-item/eg-item-service/src/main/java/com/eogo/item/web/CategoryController.java
@@ -0,0 +1,36 @@
+package com.eogo.item.web;
+
+import com.eogo.item.service.CategoryService;
+import com.eogo.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据父节点查询商品类目
+     * @param pid
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryByParentId(
+            @RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        List<Category> list = this.categoryService.queryListByParent(pid);
+        if (list == null || list.size() < 1) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(list);
+    }
+}
