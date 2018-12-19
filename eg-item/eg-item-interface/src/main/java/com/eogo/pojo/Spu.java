0ff0e7433526da2490399ece214acf38f5ec9546
zhangxiaohui4565 <zhangxh940426@163.com>
2018-12-19 22:40:57 +0800

    增加spu商品实体类

---
+++ eg-item/eg-item-interface/src/main/java/com/eogo/pojo/Spu.java
@@ -0,0 +1,45 @@
+package com.eogo.pojo;
+
+import lombok.Data;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_spu")
+@Data
+public class Spu {
+    //自增主键
+    @Id
+    @KeySql(useGeneratedKeys = true)
+    private  Long id ;
+    private Long brandId;
+    /**
+     * 一级类目
+     */
+    private Long cid1;
+    /**
+     * 二级类目
+     */
+    private Long cid2;
+    /**
+     * 三级类目
+     */
+    private Long cid3;
+    private String title;
+    /**
+     * 子标题
+     */
+    private String subTitle;
+    /**
+     * 是否下架
+     */
+    private Boolean saleable;
+    /**
+     * 逻辑删除字段
+     */
+    private Boolean valid ;
+    private Date createTime;
+    private Date lastUpdateTime;
+}
---
+++ eg-item/eg-item-interface/src/main/java/com/eogo/pojo/SpuDetail.java
@@ -0,0 +1,36 @@
+package com.eogo.pojo;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+@Data
+public class SpuDetail {
+    @Id
+    private Long spuId ;
+    private String description ;
+    /**
+     * 商品特殊规格的名称及可选值魔板
+     * {"4":["白色","金色","玫瑰金"],"12":["3GB"],"13":["16GB"]}
+     */
+    private String specialSpec ;
+    /**
+     * 商品的全局规格属性
+     * {"1":"其它","2":"G9青春版（全网通版）","3":2016.0,"5":143,"6":"其它","7":"Android","8":"骁龙（Snapdragon)",
+     * "9":"骁龙617（msm8952）","10":"八核","11":1.5,"14":5.2,"15":"1920*1080(FHD)","16":800.0,"17":1300.0,"18":3000.0}
+     */
+    private String genericSpec;
+    /**
+     * 包装清单
+     * 手机（电池内置）*1，中式充电器*1，数据线*1，半入耳式线控耳机*1，华为手机凭证*1，快速指南*1，取卡针*1，屏幕保护膜（出厂已贴）*1
+     */
+    private String packingList;
+    /**
+     * 售后服务
+     */
+    private String afterService;
+
+
+}
